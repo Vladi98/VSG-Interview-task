cdebe2affaccfafb9877c746cf32a2c5ec8de588
Vladimir Dachkinov <vladimir.dachkinov@strypes.eu>
2023-01-24 11:50:51 +0200

    Fix db path

--- src/settings.py
+++ src/settings.py
@@ -78,10 +78,12 @@ WSGI_APPLICATION = 'src.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / "db.sqlite3"),
     }
 }
 
